6211ea6cc29eb992cc5d92c96aca9ee33c538407
Ali raza <chatwithaliraza@gmail.com>
2025-12-19 12:22:51 +0500

    Made A Admin Dashboard

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -13,6 +13,7 @@ export function AdminLayout() {
         <div className="p-6">
           <AdminNavbar />
         </div>
+        
         <div className="flex-1 p-6">
           <div className="bg-white rounded-xl shadow-md p-6 h-full">
             <Outlet/>
